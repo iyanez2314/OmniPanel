850456a0f8abfb8bd9450b832c3aeb11a9fb8ebc
Isaac Yanez <isaac231467@gmail.com>
2024-01-28 17:03:46 -0600

    changed the schema to fit the applications needs

--- omnipanel/src/app/db/schema.ts
+++ omnipanel/src/app/db/schema.ts
@@ -1,8 +1,22 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, varchar, timestamp } from "drizzle-orm/pg-core";
+import { pgEnum } from "drizzle-orm/pg-core";
 
+// ENUMS
+
+export const subscriptionEnum = pgEnum("subscription", ["free", "premium"]);
+export const accountProviderEnum = pgEnum("account_provider", [
+  "google",
+  "discord",
+  "email",
+]);
+
+// TABLES
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   fullName: text("full_name"),
   email: varchar("email", { length: 256 }),
   phone: varchar("phone", { length: 256 }),
+  subscription: subscriptionEnum("subscription"),
+  accountProvider: accountProviderEnum("account_provider"),
+  createdAt: timestamp("created_at").defaultNow(),
 });
